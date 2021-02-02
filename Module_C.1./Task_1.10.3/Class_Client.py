313d045ee74b67fca066ab055226eb0313d4d4de
Nickolas_Kayne <nik.kanygin@icloud.com>
2021-02-02 23:44:53 +0300

    02.02.21

--- Module_C.1./Task_1.10.3/Class_Client.py
+++ Module_C.1./Task_1.10.3/Class_Client.py
@@ -1,21 +1,35 @@
 #                                     ("\(O_o)/")
-client_list = [{"Name": "Ivan Petrov", "Wallet": 50.0}]
-
 class Client:
-    def __init__(self, name, wallet_balance, ):
+    def __init__(self, name="---", wallet_balance=0):
         self.name = name
         self.wallet_balance = wallet_balance
 
     def list_processing(self, client_info):
         self.name = client_info.get("Name")
-        self.wallet_balance = client_info.get("Wallet")
         if isinstance(client_info.get("Wallet"), float):
-            self.wallet_balance = "$", str(client_info.get("Wallet"))
+            self.wallet_balance = "$ " + str(client_info.get("Wallet"))
         else:
             return print("Incorrect value.", client_info.get("Wallet"))
 
     def show_info(self):
-        return f"""Client: {self.name} \nWallet balance: {self.wallet_balance}"""
+        return f"Client: {self.name} \nWallet balance: {self.wallet_balance}"
 
-    def add_client(self):
-        client_list.append({"Name": {self.name}, "Wallet": {self.wallet_balance}})
+client_list = [
+    {
+        "Name": "Ivan Petrov",
+        "Wallet": 50.0
+    },
+    {
+        "Name": "Vladimir Pupkin",
+        "Wallet": 0.99999
+    },
+    {
+        "Name": "Donald Trump",
+        "Wallet": 999999.0
+    }
+]
+
+for i in client_list:
+    c = Client()
+    c.list_processing(i)
+    print(c.show_info())
--- Module_C.1./Task_1.10.3/Object_file.py
+++
@@ -1,6 +0,0 @@
-#                                     ("\(O_o)/")
-from Class_Client import Client
-
-client_1 = Client("Vladimir", 1450.2)
-
-client_1.add_client()
